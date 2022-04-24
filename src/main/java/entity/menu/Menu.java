5aa15b7fd679fe43220a8e77f9a4165f98cfe7c0
gyoungeunbae <ruddmsdl000@naver.com>
2022-04-25 01:17:02 +0900

    menu entity 추가

---
+++ src/main/java/entity/menu/Menu.java
@@ -0,0 +1,50 @@
+package entity.menu;
+
+import entity.Audit;
+import entity.contstants.CategoryType;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.experimental.Tolerate;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Builder
+@Table(name = "menus")
+public class Menu extends Audit {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "menu_id")
+    private Long id;
+
+    // Todo:: storeId 추가
+
+    @Column(nullable = false)
+    private String name;
+
+    private CategoryType category;
+
+    private String description;
+
+    @Column(nullable = false)
+    private Long amount;
+
+    @Tolerate
+    public Menu() {}
+
+    public static Menu of(
+            String name,
+            CategoryType category,
+            String description,
+            Long amount
+    ) {
+        return builder()
+                .name(name)
+                .category(category)
+                .description(description)
+                .amount(amount)
+                .build();
+    }
+}
