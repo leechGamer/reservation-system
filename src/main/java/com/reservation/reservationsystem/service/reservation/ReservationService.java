625a04e5bc1b159c3bd60fb0cc125deef6aa8681
leechGamer <ruddmsdl000@naver.com>
2022-05-24 04:10:15 +0900

    [#39]사업자는 예약을 취소할 수 있다.

--- src/main/java/com/reservation/reservationsystem/service/reservation/ReservationService.java
+++ src/main/java/com/reservation/reservationsystem/service/reservation/ReservationService.java
@@ -1,23 +1,74 @@
 package com.reservation.reservationsystem.service.reservation;
 
 import com.reservation.reservationsystem.dto.reservation.ReservationSearchRequest;
+import com.reservation.reservationsystem.entity.contstants.ReservationStatus;
 import com.reservation.reservationsystem.entity.reservation.Reservation;
+import com.reservation.reservationsystem.exception.ErrorCode;
+import com.reservation.reservationsystem.exception.ReservationException;
 import com.reservation.reservationsystem.repository.reservation.ReservationRepository;
-import com.reservation.reservationsystem.repository.reservation.ReservationRepositoryImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class ReservationService {
+
+    final static int AVAILABLE_CANCEL_HOUR = 5;
 
     final private ReservationRepository reservationRepository;
 
     public List<Reservation> search(Long storeId, ReservationSearchRequest requestDTO) {
         List<Reservation> reservations = reservationRepository.search(storeId, requestDTO);
         return reservations;
+    }
+
+    @Transactional
+    public void cancel(Long reservationId) throws ReservationException {
+        Optional<Reservation> reservation = reservationRepository.findById(reservationId);
+        Optional.ofNullable(reservation)
+                .filter(r -> this.checkAvailableTime(r.get()))
+                .filter(r -> this.checkStatus(r.get()))
+                .ifPresentOrElse(
+                        r -> {
+                            r.get().setReservationStatus(ReservationStatus.CANCELED);
+                            r.get().setDeletedAt(LocalDateTime.now());
+                        },
+                        () -> {
+                            throw new ReservationException(ErrorCode.NOT_POSSIBLE_CANCEL_RESERVATION);
+                        }
+                );
+    }
+
+    private boolean checkAvailableTime(Reservation reservation) {
+        LocalDate date = reservation.getReservedDate();
+        LocalTime time = reservation.getReservedTime();
+        LocalDateTime reservedDateTime = LocalDateTime.of(date, time);
+
+        LocalDateTime now = LocalDateTime.now();
+
+        if (now.isAfter(reservedDateTime.minusHours(AVAILABLE_CANCEL_HOUR))
+                || now.equals(reservedDateTime.plusHours(AVAILABLE_CANCEL_HOUR))
+        ) {
+            return false;
+        }
+        return true;
+    }
+
+    private boolean checkStatus(Reservation reservation) {
+        ReservationStatus status = reservation.getReservationStatus();
+        if (status.equals(ReservationStatus.EXPIRED)
+                || status.equals(ReservationStatus.CANCELED)
+        ) {
+            return false;
+        }
+        return true;
     }
 }
 
