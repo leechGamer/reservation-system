ffadab99605d3b0ceee08a71e5c6e90f540f5dc6
leechGamer <ruddmsdl000@naver.com>
2022-06-15 23:35:35 +0900

    회원가입 버그 수정

---
+++ src/main/java/com/reservation/reservationsystem/config/WebSecurityConfig.java
@@ -0,0 +1,22 @@
+package com.reservation.reservationsystem.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/css/**", "/script/**", "image/**", "/fonts/**", "lib/**");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .httpBasic().and()
+                .authorizeRequests()
+                .antMatchers("/**").permitAll();
+    }
+}
--- src/main/java/com/reservation/reservationsystem/controller/CustomerController.java
+++ src/main/java/com/reservation/reservationsystem/controller/CustomerController.java
@@ -3,20 +3,21 @@ package com.reservation.reservationsystem.controller;
 import com.reservation.reservationsystem.dto.customer.CustomerSignUpDTO;
 import com.reservation.reservationsystem.entity.customer.Customer;
 import com.reservation.reservationsystem.service.customer.CustomerService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/auth")
+@RequiredArgsConstructor
 public class CustomerController {
 
-    private CustomerService customerService;
-
-    public CustomerController(CustomerService customerService) {
-        this.customerService = customerService;
-    }
+    private final CustomerService customerService;
 
     @PostMapping("/signup")
     public ResponseEntity register(@RequestBody @Valid CustomerSignUpDTO customerDTO) {
